ceae9cf89ecd9f9691a43f5df0221ccb7d4ff212
Berat Güdelek <beratgdlk@gmail.com>
2025-03-21 19:15:13 +0300

    knex, .env added

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,23 @@
-// Update with your config settings.
-
+// up
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-module.exports = {
+import dotenv from 'dotenv'
+dotenv.config();
+
+export default {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './src/migrations'
+    },
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
---
+++ src/migrations/20250321161245_create_categories_table.js
@@ -0,0 +1,7 @@
+export async function up(knex) {
+    
+} /* migrate edildiğinde çalışacak kod */
+
+export async function down(knex) {
+    
+} /* migrate çekildiğinde çalışacak kod */
