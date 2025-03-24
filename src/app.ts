2381f66040e01eac6258d1248117f397beaece89
Berat Güdelek <beratgdlk@gmail.com>
2025-03-24 15:27:07 +0300

    categoryModel

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { Console } from 'console';
+import categoryRoutes from './routes/categoryRoutes.js';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use('/api/categories', categoryRoutes)
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server Standing!!!')      
--- src/models/categoryModel.ts
+++ src/models/categoryModel.ts
@@ -3,5 +3,21 @@ import { db } from '../config/database.js'
 export const getAllCategories = () => {
     const query = db('categories')
     return query;
-    
+
+}
+
+export const getCategoryById = (id:number)=>{
+    return db('categories').where({ id }).first();
+}
+
+export const createCategory = (data:object)=>{
+    return db('categories').insert(data).returning('*');
+}
+
+export const updateCategory = (id:number, data:object) => {
+    return db('categories').where({id}).update(data).returning('*');
+}
+
+export const deleteCategory = (id:number) => {
+    return db('categories').where({id}).update({deleted_at: new Date()}).returning('*');
 }
